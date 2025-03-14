e8dd580601eddf4bcf445073389cfb54d908bf1c
Rafael Monteiro <161628330+RafaelMonterio@users.noreply.github.com>
2025-03-14 08:44:57 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2F, sou o leo</p>
+
+    <?php
+        $name = "leo";
+        echo "<p>Oi 2F, sou o leo</p>";
+        echo "<p>Oi 2F, sou o $name</p>";
+        echo "<p>Oi 2F, sou o ".$name."</p>";
+     ?>
+
+</body>
+</html>
